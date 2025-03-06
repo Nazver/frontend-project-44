2e48562d738fcba42dde0f90dc76f4eff0786b09
nazver <jeka050616@gmail.com>
2025-03-07 00:37:22 +0700

    エイトstep

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -2,10 +2,39 @@
 import readlineSync from 'readline-sync';
 import welcome, { randomNumber } from '../src/cli.js';
 
-export default function gameProgression() {
-  const count = 0;
+export default function gameProgression(name) {
   console.log('What number is missing in the progression?');
-  while (count < 3) {
 
+  let counter = 0;
+
+  while (counter < 3) {
+    const length = randomNumber(5, 10);
+    const start = randomNumber(1, 10);
+    const step = randomNumber(1, 10);
+    const progression = [];
+
+    for (let i = 0; i < length; i += 1) {
+      progression.push(start + step * i);
+    }
+
+    const random1 = randomNumber(0, progression.length - 1);
+    const correct = progression[random1];
+    progression[random1] = '..';
+
+    console.log(`Quesion: ${progression.join(' ')}`);
+
+    const answer = readlineSync.question('Your answer: ');
+
+    if (correct.toString() !== answer.toString()) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+
+    counter += 1;
+    console.log('Correct');
   }
+
+  console.log(`Congratulations, ${name}`);
 }
+gameProgression(welcome());
